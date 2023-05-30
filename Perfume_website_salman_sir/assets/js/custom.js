8ee7dbc2facbd5e867e7bf5c1519817133f04c9a
asalaminbd <119441279+asalaminbd@users.noreply.github.com>
2023-05-30 08:13:21 +0600

    Add files via upload

---
+++ Perfume_website_salman_sir/assets/js/custom.js
@@ -0,0 +1,72 @@
+$(document).ready(()=>{
+			$('.multiple-items').slick({
+				dots: true,
+				infinite: true,
+				speed: 300,
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				},
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
+});
+		});
+
+
+
+
+
+
+
+
+		$(document).ready(()=>{
+			$('.center-slid').slick({
+				centerMode: true,
+				centerPadding: '60px',
+				slidesToShow: 7,
+				responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 7
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						arrows: false,
+						centerMode: true,
+						centerPadding: '40px',
+						slidesToShow: 2
+					}
+				}
+				]
+			});
+		});
